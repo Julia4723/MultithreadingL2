6e5ab863d6fe30344a6ab942d3e4ac4523cdfc3b
My <uk472363@yanex.ru>
2025-05-24 10:08:14 +0300

    fix

--- MultithreadingL2/ViewControllers/ComplexRaceConditionViewController.swift
+++ MultithreadingL2/ViewControllers/ComplexRaceConditionViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ThreadSafeArray<T> {
     private var array: [T] = []
     private let lock = NSLock()
-  
+    
     
     func append(_ value: T) {
         lock.lock()
@@ -30,15 +30,18 @@ class ThreadSafeArray<T> {
 
 final class ComplexRaceConditionViewController: UIViewController {
     private var counter = 0
+    private let lock = NSLock()
     private var log: ThreadSafeArray<String> = ThreadSafeArray()
     
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let thread1 = Thread {
             for i in 0..<1000 {
+                self.lock.lock()
                 self.counter += 1
+                self.lock.unlock()
                 self.log.append("Thread 1 - step \(i)")
             }
             print("Thread 1 finished")
@@ -46,8 +49,10 @@ final class ComplexRaceConditionViewController: UIViewController {
         
         let thread2 = Thread {
             for i in 0..<1000 {
+                self.lock.lock()
                 self.counter += 1
-                self.log.append("Thread 1 - step \(i)")
+                self.lock.unlock()
+                self.log.append("Thread 2 - step \(i)")
             }
             print("Thread 2 finished")
         }
@@ -58,7 +63,7 @@ final class ComplexRaceConditionViewController: UIViewController {
         Thread.sleep(forTimeInterval: 2)
         
         print("Final counter: \(counter) (ожидаем 2000)")
-        print("Log count: \(log.snapshot()) (ожидаем тоже 2000, но будет меньше или краш)")
+        print("Log count: \(log.snapshot().count) (ожидаем тоже 2000, но будет меньше или краш)")
     }
 }
 
